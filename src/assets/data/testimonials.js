a0080cd8a8210c2ea00a71b46056fe1e94dfa9ac
Rinzler8806 <shane.rogers916@gmail.com>
2021-08-17 19:06:38 -0700

    added testimonials section and data file with testimonials. Added switch transitions for each testimonial

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,27 +1,27 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
-    title: 'CEO',
-    org: 'WEB CIFAR',
+    name: 'Ahmed Ali',
+    title: 'Project Co-Author',
+    org: 'State of California',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Shane is an outstanding Software Developer! In the past I have had the pleasure of collaborating with Shane on multiple projects. I am compelled to write this recommendation simply because Shanes contribution always made a big impact. He takes the lead on projects and manages team collaboration in a fun and engaging way. Shane is very creative and provides lots of innovative ideas. He is driven and possesses high urgency to completing tasks in a meticulous fashion. Shane is agile when finding solutions to problems. I highly recommend Shane, he is a great contributor to any team!',
   },
   {
     id: 2,
-    name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
+    name: 'Uyen Nguyen',
+    title: 'Fullstack Web Developer',
+    org: 'Dovetail Genomics',
     desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+      'Shane had proven to be both a talented and dedicated professional. He demonstrated great knowledge in his field and always brought positive energy to everyone around. During group discussions, I was really impressed with his creative way of approaching and solving problems. I have learned a lot from Shane, and I am looking forward to collaborating with him in the future projects. I highly recommend Shane as a team members whose great work will help other members strive for better results.',
   },
   {
     id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
+    name: 'Keith Sutton',
+    title: 'Senior Firmware Engineer',
+    org: 'Hewlett-Packard',
     desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      'I had the honor to work with Shane on several projects for the 2020-2021 UC Davis Full Stack Developer Certificate Program. Our final project, Pangea Chat, integrated all of the software development areas for the client and server that we studied. Although our team of four members had no formal hierarchy, Shane quickly took the role of lead engineer. He organized and directed our team throughout the projects conception, design, development, test, deployment, and presentation phases. I highly recommend Shane for any full stack engineer position that requires knowledge and experience with HTTP, CSS, JavaScript, jQuery, Express.js, React.js, Node.js, MongoDB, MySQL, Git, and Heroku.',
   },
 ];
 export default testimonials;
